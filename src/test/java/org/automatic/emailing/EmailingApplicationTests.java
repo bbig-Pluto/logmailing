faa14f68513694422962976a2fc02bd67337b851
bbig-Pluto <xkdxkd011@gmail.com>
2023-01-04 09:11:02 +0900

    first

---
+++ src/test/java/org/automatic/emailing/EmailingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.automatic.emailing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
